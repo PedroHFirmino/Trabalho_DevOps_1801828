17fa0110c0b1b0c9df55cf8bce8fe11695e8f34b
Pedro Firmino <pedrohefirmino@gmail.com>
2024-12-01 15:25:18 -0300

    ./app

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
-from app.routes import main
+from app.routes import init_app
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@db/alunos_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Importa rotas
-app.register_blueprint(main)
+# Inicializar as rotas
+init_app(app)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
